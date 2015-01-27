96654040fea8fe13fb0fad2b92c25758223c8680
KM <you@example.com>
2015-01-27 15:27:51 +0200

    MiniMarket 2

--- MiniMarket/public/mainmodule.js
+++ MiniMarket/public/mainmodule.js
@@ -5,11 +5,34 @@ mainMod.config(function($routeProvider, $locationProvider){
     $locationProvider.html5mode(true);
     
     $routeProvider.when('/',{
-        templateUrl:'products.html'
+        templateUrl:'products.html',
+        controller:'ProductController'
     });
     
     $routeProvider.when('/add',{
         templateUrl:'userproduct.html'
     });
     
+});
+
+mainMod.controller('ProductController',function($scope,ProductFactory){
+    
+    $scope.products = ProductFactory.getProducts($scope);
+});
+
+mainMod.factory('ProductFactory',function($http){
+    
+    var factory = {};
+    factory.getProducts = function($scope){
+        
+        $http.get('/data').success(function(data, status, headers, config){
+            //console.log(data);
+            $scope.products = data;
+        }).
+        error(function(data, status, headers, config){
+            console.log('error loading data');
+        });
+    }
+    
+    return factory;
 });
--- MiniMarket/public/user.js
+++ MiniMarket/public/user.js
@@ -1,12 +1,20 @@
 var express = require('express');
 
-var router = express.Route();
+var router = express.Router();
 
-router.get(function(req,res){
-    var dummy = [{name:'maito',price:'1,75 €'},{name:'coffee',price:'3,45 €'}];
+router.get('/',function(req,res){
+    var dummy = [{name:'maito',price:'1,75 €'},{name:'kahvi',price:'3,45 €'}];
     
     res.send(dummy);
 });
 
-router.post(function(req,res){
+router.post('/',function(req,res){
 });
+
+router.put('/',function(req,res){
+});
+
+router.delete('/',function(req,res){
+});
+
+module.exports = router;
